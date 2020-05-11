8b79ed6a405ca80bb4a1382f77a0985760598697
kikeex <joseoberto2@gmail.com>
2020-05-10 19:23:20 -0500

    front & fix consult

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = ({ percentage }) => {
+  return (
+    <div className="footer-copyright text-center py-3 footer">
+    
+    </div>
+  );
+};
+
+export default Footer;
--- src/app.js
+++ src/app.js
@@ -2,10 +2,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const cors = require('cors');
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 app.use('/', routes);
 
 module.exports = app
