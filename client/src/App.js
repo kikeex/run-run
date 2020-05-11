ef8758394317ae9805ddee381fd8cbaf9d0d00bc
kikeex <joseoberto2@gmail.com>
2020-05-11 10:15:21 -0500

    fix alt

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 const App = () => (
   <div className='container mt-4' >
     <h4 className='display-4 text-center mb-4'>
-      <img src={Logo} className="logo-image" alt="Chiper logo" />
+      <img src={Logo} className="logo-image" alt="RunRun logo" />
     </h4>
     <SearchForm />
     <Footer />
